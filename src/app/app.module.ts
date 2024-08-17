c3d3c182df9c9424816ec897ccaf4127968951b8
Fsxerath <frediangarita1999@gmail.com>
2024-08-17 16:30:00 -0500

    generate all atoms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimaryButtonComponent } from './components/atoms/primary-button/primary-button.component';
+import { UserIconComponent } from './components/atoms/user-icon/user-icon.component';
+import { LabelComponent } from './components/atoms/label/label.component';
+import { InputComponent } from './components/atoms/input/input.component';
+import { InputsComponent } from './components/atoms/inputs/inputs.component';
+import { LabelsComponent } from './components/atoms/labels/labels.component';
+import { TitlesComponent } from './components/atoms/titles/titles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaryButtonComponent,
+    UserIconComponent,
+    LabelComponent,
+    InputComponent,
+    InputsComponent,
+    LabelsComponent,
+    TitlesComponent
   ],
   imports: [
     BrowserModule,
