3a02e46fdb7586d2f1414af73b3ea82f6033241e
Fsxerath <frediangarita1999@gmail.com>
2024-08-22 16:49:34 -0500

    add molecule to show information

--- src/app/components/organisms/show-series/show-series.component.ts
+++ src/app/components/organisms/show-series/show-series.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ShowSeries } from 'src/app/data/interfaces/show-series.interface';
 
 @Component({
   selector: 'app-show-series',
@@ -6,5 +7,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./show-series.component.scss']
 })
 export class ShowSeriesComponent {
-  @Input() series_img !: string;
+  series: ShowSeries;
+  details: boolean = false;
+  constructor() { 
+    this.series = {
+      series_img: 'https://th.bing.com/th/id/OIP.PuC7GBD2hHxHnowUvrnxQgAAAA?rs=1&pid=ImgDetMain',
+      chapters: 145,
+      progress: 2,
+      date: 'N/A',
+      synopsis: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At deleniti atque omnis, est repudiandae quisquam harum assumenda eaque nam distinctio fugit eos nisi, voluptatum, ullam aut fugiat. Error, eos ea!'
+    }
+  }
+  showDetails() {
+    this.details = !this.details;
+  }
 }
---
+++ src/app/data/interfaces/show-series.interface.ts
@@ -0,0 +1,7 @@
+export interface ShowSeries {
+    series_img: string;
+    chapters: number;
+    progress: number;
+    date: string;
+    synopsis: string;
+}
