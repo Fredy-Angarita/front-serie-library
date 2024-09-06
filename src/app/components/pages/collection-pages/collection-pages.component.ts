d12b2cd2e3574d373faca8e4e15f22eebd99f37e
Fsxerath <frediangarita1999@gmail.com>
2024-09-06 12:54:57 -0500

    add routes

---
+++ src/app/components/pages/collection-pages/collection-pages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-pages',
+  templateUrl: './collection-pages.component.html',
+  styleUrls: ['./collection-pages.component.scss']
+})
+export class CollectionPagesComponent {
+
+}
