a12a74871089411ef3d8d2348de21476c95a7896
Fsxerath <frediangarita1999@gmail.com>
2024-09-08 15:59:28 -0500

    feat(login): implement the auth services

--- src/app/components/organisms/login-form/login-form.component.ts
+++ src/app/components/organisms/login-form/login-form.component.ts
@@ -1,11 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { LABELS, PLACEHOLDERS, QUESTION_ACTIONS, TITLES, TYPE_BUTTONS } from 'src/app/data/constants/constants';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, EventEmitter, Inject, Output, PLATFORM_ID } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  LABELS,
+  PLACEHOLDERS,
+  QUESTION_ACTIONS,
+  TITLES,
+  TYPE_BUTTONS,
+} from 'src/app/data/constants/constants';
+import { PostLoginRequest } from 'src/app/data/services/auth/login/dtos/request/post.login.request';
+import { AuthLoginService } from 'src/app/data/services/auth/login/services/auth.login.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
   loginForm: FormGroup;
@@ -16,13 +31,23 @@ export class LoginFormComponent {
   password_placeholder = PLACEHOLDERS.PASSWORD;
   title = TITLES.LOGIN;
   question = QUESTION_ACTIONS.NOT_HAVE_ACCOUNT;
-  action = QUESTION_ACTIONS.REGISTER_ACTION;  
+  action = QUESTION_ACTIONS.REGISTER_ACTION;
+  isBrowser !: boolean;
   @Output() changeEvent = new EventEmitter<boolean>();
 
-  constructor( private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private loginService: AuthLoginService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
     this.loginForm = this.fb.nonNullable.group({
-      email: ['',[Validators.required, Validators.email, Validators.minLength(6)]],
-      password: ['',[Validators.required, Validators.minLength(8)]]
+      email: [
+        '',
+        [Validators.required, Validators.email, Validators.minLength(6)],
+      ],
+      password: ['', [Validators.required, Validators.minLength(8)]],
     });
   }
   get emailControl() {
@@ -35,12 +60,23 @@ export class LoginFormComponent {
     if (this.loginForm.invalid) {
       return;
     }
-    console.log('form works');
+    this.login();
   }
   onValueChange(controlName: string, value: string) {
     this.loginForm.get(controlName)?.setValue(value);
   }
   onChangeEvent(booleanSwitch: boolean) {
     this.changeEvent.emit(booleanSwitch);
+  }
+  login() {
+    const { email, password } = this.loginForm.value;
+    this.loginService.login({ email, password } as PostLoginRequest).subscribe({
+      next: (response) => {
+        if (this.isBrowser) {
+          this.loginService.saveAccessToken(response.access_token);
+        }
+        this.router.navigate(['/collection']);
+      },
+    });
   }
 }
