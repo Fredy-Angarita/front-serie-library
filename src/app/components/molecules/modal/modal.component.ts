37f228cc69e914f998871c2a173ee04071a52fd8
Fsxerath <frediangarita1999@gmail.com>
2024-09-23 16:36:32 -0500

    feature (modal) add emitter to close modal

--- src/app/components/molecules/modal/modal.component.ts
+++ src/app/components/molecules/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,4 +7,10 @@ import { Component, Input } from '@angular/core';
 })
 export class ModalComponent {
   @Input() title!: string;
+  @Output() close = new EventEmitter<void>();
+
+  closeModal(){
+    this.close.emit();
+  }
+
 }
