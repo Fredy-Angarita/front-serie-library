c4f363170502f1dc67403ac93a9b9876eb450ba9
Fsxerath <frediangarita1999@gmail.com>
2024-10-18 20:29:26 -0500

    refactor (template-module): move template-components into template-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,24 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SeriesTemplateComponent } from './components/templates/series-template/series-template.component';
-import { UserFormsComponent } from './components/templates/user-forms/user-forms.component';
-import { CollectionTemplateComponent } from './components/templates/collection-template/collection-template.component';
 import { LoginPagesComponent } from './components/pages/login-pages/login-pages.component';
 import { RegisterPagesComponent } from './components/pages/register-pages/register-pages.component';
 import { CollectionPagesComponent } from './components/pages/collection-pages/collection-pages.component';
 import { SeriesPagesComponent } from './components/pages/series-pages/series-pages.component';
-import { NotFoundComponent } from './components/templates/not-found/not-found.component';
 import { NotFoundPagesComponent } from './components/pages/not-found-pages/not-found-pages.component';
 import { TokenInterceptor } from './data/services/auth/login/interceptors/token.interceptor';
 import { LibraryPagesComponent } from './components/pages/library-pages/library-pages.component';
-import { LibraryTemplateComponent } from './components/templates/library-template/library-template.component';
-import { AtomsModule } from './shared/components/atoms/atoms.module';
-import { MoleculesModule } from './shared/components/molecules/molecules.module';
 import { OrganismsModule } from './shared/components/organisms/organisms.module';
+import { TemplatesModule } from './shared/components/templates/templates.module';
 @NgModule({
   declarations: [
     AppComponent,
-    SeriesTemplateComponent,
-    UserFormsComponent,
-    CollectionTemplateComponent,
     LoginPagesComponent,
     RegisterPagesComponent,
     CollectionPagesComponent,
     SeriesPagesComponent,
-    NotFoundComponent,
     NotFoundPagesComponent,
     LibraryPagesComponent,
-    LibraryTemplateComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,9 +30,8 @@ import { OrganismsModule } from './shared/components/organisms/organisms.module'
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AtomsModule,
-    MoleculesModule,
     OrganismsModule,
+    TemplatesModule,
   ],
   providers: [
     {
--- src/app/shared/components/templates/templates.module.ts
+++ src/app/shared/components/templates/templates.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { CollectionTemplateComponent } from './collection-template/collection-template.component';
+import { LibraryTemplateComponent } from './library-template/library-template.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SeriesTemplateComponent } from './series-template/series-template.component';
+import { UserFormsComponent } from './user-forms/user-forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { OrganismsModule } from '../organisms/organisms.module';
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    CollectionTemplateComponent,
+    LibraryTemplateComponent,
+    NotFoundComponent,
+    SeriesTemplateComponent,
+    UserFormsComponent,
+  ],
+  exports: [
+    CollectionTemplateComponent,
+    LibraryTemplateComponent,
+    NotFoundComponent,
+    SeriesTemplateComponent,
+    UserFormsComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, OrganismsModule],
 })
-export class TemplatesModule { }
+export class TemplatesModule {}
