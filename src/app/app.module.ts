73234c70ffc41f5e353daa4d32fbfeb6deecd960
Fsxerath <frediangarita1999@gmail.com>
2024-10-18 19:56:26 -0500

    refactor (organisms-module): move organisms-components into organims-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,6 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShowSeriesComponent } from './components/organisms/show-series/show-series.component';
-import { NavBarComponent } from './components/organisms/nav-bar/nav-bar.component';
-import { ListSummaryComponent } from './components/organisms/list-summary/list-summary.component';
-import { LoginFormComponent } from './components/organisms/login-form/login-form.component';
-import { RegisterFormComponent } from './components/organisms/register-form/register-form.component';
 import { SeriesTemplateComponent } from './components/templates/series-template/series-template.component';
 import { UserFormsComponent } from './components/templates/user-forms/user-forms.component';
 import { CollectionTemplateComponent } from './components/templates/collection-template/collection-template.component';
@@ -20,21 +15,14 @@ import { SeriesPagesComponent } from './components/pages/series-pages/series-pag
 import { NotFoundComponent } from './components/templates/not-found/not-found.component';
 import { NotFoundPagesComponent } from './components/pages/not-found-pages/not-found-pages.component';
 import { TokenInterceptor } from './data/services/auth/login/interceptors/token.interceptor';
-import { ProgressFormComponent } from './components/organisms/progress-form/progress-form.component';
 import { LibraryPagesComponent } from './components/pages/library-pages/library-pages.component';
-import { CollectionOrganismsComponent } from './components/organisms/collection-organisms/collection-organisms.component';
 import { LibraryTemplateComponent } from './components/templates/library-template/library-template.component';
-import { SeriesFormComponent } from './components/organisms/series-form/series-form.component';
 import { AtomsModule } from './shared/components/atoms/atoms.module';
 import { MoleculesModule } from './shared/components/molecules/molecules.module';
+import { OrganismsModule } from './shared/components/organisms/organisms.module';
 @NgModule({
   declarations: [
     AppComponent,
-    ShowSeriesComponent,
-    NavBarComponent,
-    ListSummaryComponent,
-    LoginFormComponent,
-    RegisterFormComponent,
     SeriesTemplateComponent,
     UserFormsComponent,
     CollectionTemplateComponent,
@@ -44,11 +32,8 @@ import { MoleculesModule } from './shared/components/molecules/molecules.module'
     SeriesPagesComponent,
     NotFoundComponent,
     NotFoundPagesComponent,
-    ProgressFormComponent,
     LibraryPagesComponent,
-    CollectionOrganismsComponent,
     LibraryTemplateComponent,
-    SeriesFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -58,6 +43,7 @@ import { MoleculesModule } from './shared/components/molecules/molecules.module'
     HttpClientModule,
     AtomsModule,
     MoleculesModule,
+    OrganismsModule,
   ],
   providers: [
     {
